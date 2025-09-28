06a588040d1681342e277592fa135e591b9415f9
hmsami4547 <hmsami4547@gmail.com>
2025-09-28 14:30:51 +0600

    added heart button changing

--- home.js
+++ home.js
@@ -1,1 +1,59 @@
-console.log('hi you are connected')
+
+// document.getElementById('heart').addEventListener('click',function(
+// e){
+//     e.preventDefault();
+//     const redHeart = document.getElementById('red-heart')
+//     const redHeartValue = parseInt(redHeart.innerText)
+//     const totalRedHeart = redHeartValue + 1;
+    
+//    redHeart.innerText = totalRedHeart;
+
+//     return redHeart;
+// })
+function addOne(){
+     
+    const redHeart = document.getElementById('red-heart')
+    const redHeartValue = parseInt(redHeart.innerText)
+    const totalRedHeart = redHeartValue + 1;
+    
+   redHeart.innerText = totalRedHeart;
+
+    return redHeart;
+}
+
+document.getElementById('heart1').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart2').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart3').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart4').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart5').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart6').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart7').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart8').addEventListener('click',function(
+){
+    addOne();
+})
+document.getElementById('heart9').addEventListener('click',function(
+){
+    addOne();
+})
